41dc5134523362c28493ec774ef8215ec2298d8d
RParadeda <raulparadeda@uern.br>
2025-02-04 12:58:11 -0300

    Configura CORS para aceitar requisições da origem específica e responder a preflight requests

--- server.js
+++ server.js
@@ -19,12 +19,6 @@ const corsOptions = {
 };
 app.use(cors(corsOptions));
 
-// Middleware para log de requisições
-app.use((req, res, next) => {
-  console.log(`${req.method} ${req.path}`);
-  next();
-});
-
 // Responder a preflight requests
 app.options('*', cors(corsOptions));
 
