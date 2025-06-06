81adb09d42fcf6549c0ea43a0dfeb7b8530cfc9e
FlavioFernades <flavioifrn@gmail.com>
2025-06-06 14:47:33 -0300

    Update index.js

--- src/models/index.js
+++ src/models/index.js
@@ -1,17 +1,21 @@
+require('dotenv').config(); // se ainda não adicionou
 
-
-const { Sequelize } = require('sequelize');
+const { Sequelize, DataTypes } = require('sequelize');
 
 const sequelize = new Sequelize(
   process.env.DB_NAME,
   process.env.DB_USER,
   process.env.DB_PASS,
   {
-     host: process.env.DB_HOST,
+    host: process.env.DB_HOST,
     port: process.env.DB_PORT || 3306,
     dialect: 'mysql',
   }
 );
 
-module.exports = { sequelize };
+const db = {};
+db.Sequelize = Sequelize;
+db.sequelize = sequelize;
+db.Livro = require('./livro')(sequelize, DataTypes);
 
+module.exports = db;
