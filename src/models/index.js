95f23de66ecf5daaacfaa217a126999aea7586c3
FlavioFernades <flavioifrn@gmail.com>
2025-06-05 23:31:41 -0300

    Update index.js

--- src/models/index.js
+++ src/models/index.js
@@ -7,7 +7,7 @@ const sequelize = new Sequelize(
   process.env.DB_USER,
   process.env.DB_PASS,
   {
-    host:/,  // não 'localhost' nem '127.0.0.1' se estiver em container diferente
+    host:mysql://root:pNEuOjjfXvDHndkkwxNndYnJkxdyXoJd@mysql.railway.internal:3306/railway // não 'localhost' nem '127.0.0.1' se estiver em container diferente
     dialect: 'mysql',
     port: process.env.DB_PORT || 3306,
   }
